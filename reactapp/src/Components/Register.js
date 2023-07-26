309011874d17c6a4b67a44ade0d5c9b674149f86
Szajkoggg <116949920+Szajkoggg@users.noreply.github.com>
2023-07-26 14:32:37 +0200

    add register fields

--- reactapp/src/Components/Register.js
+++ reactapp/src/Components/Register.js
@@ -1,27 +1,88 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useState, useEffect } from "react";
 
-const Register = () => {
-  return <div></div>/*<div>Register
-    <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            Password
-            <TextField
-              id="password"
-              label="Password"
-              type="password"
-              value={password}
-              onChange={handlePasswordChange}
-              error={passwordError}
-              helperText={passwordError ? "Incorrect password!" : ""}
-            />
-          </div>
-  </div>;*/
+const Register = (props) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [secondPassword, setSecondPassword] = useState("");
+  const [usernameError, setUsernameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [secondPasswordError, setSecondPasswordError] = useState(false);
+
+  const handleSubmit = () => {};
+
+  return (
+    <div>
+      Register
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Password
+          <TextField
+            id="username"
+            label="Username"
+            type="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            error={usernameError}
+            helperText={usernameError ? "Incorrect Username!" : ""}
+          />
+          <TextField
+            id="email"
+            label="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            error={emailError}
+            helperText={emailError ? "Incorrect email!" : ""}
+          />
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            error={passwordError}
+            helperText={passwordError ? "Incorrect password!" : ""}
+          />
+          <TextField
+            id="secondPassword"
+            label="Confirm Password"
+            type="password"
+            value={secondPassword}
+            onChange={(e) => setSecondPassword(e.target.value)}
+            error={secondPasswordError}
+            helperText={secondPasswordError ? "Incorrect second password!" : ""}
+          />
+        </div>
+        <Button variant="contained" type="submit">
+          Register
+        </Button>
+      </Box>
+      <Button variant="contained" onClick={() => props.onBackClick("initial")}>
+        Back
+      </Button>
+    </div>
+  );
 };
 
 export default Register;
