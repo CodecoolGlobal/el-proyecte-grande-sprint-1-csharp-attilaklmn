8f19894ae20c0296ab679ef8df4945c9f08a03cf
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-07-26 13:33:38 +0200

    implement movie list page

---
+++ reactapp/src/Pages/MovieList/MovieList.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import MovieCard from "../../Components/MovieCard/MovieCard";
+
+const MovieList = () => {
+    const [movieList, setMovieList] = useState([]);
+
+    useEffect(() => {
+        fetch("/list")
+        .then((res) => res.json())
+        .then((movieList) => setMovieList(movieList));
+    }, []);
+
+    return <div id="movieList">{<MovieCard movieList={movieList}/>}</div>
+
+};
+
+export default MovieList;
