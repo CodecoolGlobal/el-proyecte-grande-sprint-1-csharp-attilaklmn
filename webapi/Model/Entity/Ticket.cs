c5da0ff4c275af9dc848a2035ee695e1cffedfc5
attilaklmn <kalmanattila04@gmail.com>
2023-08-08 11:34:24 +0200

    create User model class

--- webapi/Model/Entity/Ticket.cs
+++ webapi/Model/Entity/Ticket.cs
@@ -8,6 +8,7 @@ namespace webapi.Model.Entity
         public long Id { get; set; }
         public Screening Screening { get; set; } = null!;
         public Seat Seat { get; set; } = null!;
+        public User User { get; set; } = null!;
         public bool Finalized { get; set; }
 
         public void SetFinalized()
---
+++ webapi/Model/Entity/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.Model.Entity
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
