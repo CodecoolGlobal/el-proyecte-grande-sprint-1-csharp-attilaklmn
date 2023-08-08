e01a6cce1c3b6400f3439bc0bbd161dd09f205a9
attilaklmn <kalmanattila04@gmail.com>
2023-08-08 11:08:39 +0200

    refactor movie model class

--- webapi/Model/Movie.cs
+++ webapi/Model/Movie.cs
@@ -1,19 +1,14 @@
-﻿namespace webapi.Model;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.Model;
 
 public class Movie
 {
-    public Guid Id { get; }
-    public string Title { get; set; }
-    public string[] Cast { get; set; }
-    public string Summary { get; set; }
-    public double UserRating { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; }
+    public string Title { get; set; } = null!;
+    public string[]? Cast { get; set; }
+    public string? Summary { get; set; }
+    public double? UserRating { get; set; }
 
-    public Movie(string title, string[] cast, string summary)
-    {
-        Id = Guid.NewGuid();
-        Title = title;
-        Cast = cast;
-        Summary = summary;
-        UserRating = 0;
-    }
 }
