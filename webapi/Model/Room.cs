91fc6b318d0ef02a2a188ae59f9e34fbf8033303
attilaklmn <kalmanattila04@gmail.com>
2023-08-08 11:24:33 +0200

    refactor Room model class

--- webapi/Model/Room.cs
+++ webapi/Model/Room.cs
@@ -1,35 +1,17 @@
-﻿namespace webapi.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webapi.Model
 {
     public class Room
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string Name { get; set; } = null!;
         public int RowNumber { get; set; }
         public int ColumnNumber { get; set; }
-        public Dictionary<int, HashSet<Seat>> Seats { get; set; }
+        public ICollection<Seat> Seats { get; set; } = null!;
 
-        public Room(string name, int rowNumber, int columnNumber)
-        {
-            Id = Guid.NewGuid();
-            Name = name;
-            RowNumber = rowNumber;
-            ColumnNumber = columnNumber;
-            Seats = new();
-            CreateSeats(rowNumber, columnNumber);
-        }
-
-        private void CreateSeats(int row, int column)
-        {
-            for (int i = 1; i <= row; i++)
-            {
-                HashSet<Seat> seatRow = new();
-                for (int j = 1; j <= column; j++)
-                {
-                    seatRow.Add(new Seat(Id, i, j));
-                }
-                Seats.Add(i, seatRow);
-            }
-        }
     }
 
 
