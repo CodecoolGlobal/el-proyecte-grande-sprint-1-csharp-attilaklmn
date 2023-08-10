9768b45763f480b4e46639b95fbbb99e1ba8365f
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-08-10 10:50:36 +0200

    implement form component

--- reactapp/src/Components/MovieList/MovieList.jsx
+++ reactapp/src/Components/MovieList/MovieList.jsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard/MovieCard";
+import MovieForm from "./MovieForm/MovieForm";
 
 const MovieList = () => {
-    const [movieList, setMovieList] = useState([]);
+  const [movieList, setMovieList] = useState([]);
 
-    useEffect(() => {
-        fetch("/list")
-        .then((res) => res.json())
-        .then((movieList) => setMovieList(movieList));
-    }, []);
+  useEffect(() => {
+    fetch("/list")
+      .then((res) => res.json())
+      .then((movieList) => setMovieList(movieList));
+  }, []);
 
-    return <div id="movieList">{<MovieCard movieList={movieList}/>}</div>
-
+  return (
+    <>
+      <div id="movieList">{<MovieCard movieList={movieList} />}</div>
+      <div id="movieForm">{<MovieForm movieList={movieList} setMovieList={setMovieList} />}</div>
+    </>
+  );
 };
 
 export default MovieList;
