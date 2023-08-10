615a4d2cbc21b6e8fbd29e32c42fa33633313dd9
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-08-10 10:50:16 +0200

    create movieForm component

---
+++ reactapp/src/Components/MovieList/MovieForm/MovieForm.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+
+const MovieForm = ({ movieList, setMovieList }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const movieData = {
+      title: "",
+      cast: [],
+      summary: "",
+    };
+
+    for (const entry of formData.entries()) {
+      if (entry[0] === "title") {
+        movieData.title = entry[1];
+      }
+      if (entry[0] === "cast") {
+        movieData.cast = entry[1].split(", ");
+      }
+      if (entry[0] === "summary") {
+        movieData.summary = entry[1];
+      }
+    }
+
+    fetch(`/list`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(movieData),
+    })
+      .then((res) => res.json())
+      .then((movie) => setMovieList([...movieList, movie]));
+
+      document.getElementById("movie-form").reset();
+  };
+
+  return (
+    <>
+      <div className="form-container">
+        <div className="movie-details">
+          <form id="movie-form" onSubmit={handleSubmit}>
+            <div className="movie-form-row">
+              <span className="movie-form-label">Title: </span>
+              <input className="movie-form-input" name="title" />
+            </div>
+            <div className="movie-form-row">
+              <span className="movie-form-label">Cast: </span>
+              <input className="movie-form-input" name="cast" />
+            </div>
+            <div className="movie-form-row">
+              <span className="movie-form-label">Summary: </span>
+              <input className="movie-form-input" name="summary" />
+            </div>
+            <div className="button-row">
+              <button className="save-button" type="submit">
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MovieForm;
