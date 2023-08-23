7e3a6a995648ce178c13220a22c4b589f0fc8e55
attilaklmn <kalmanattila04@gmail.com>
2023-08-23 09:30:53 +0200

    add seatId and userId to ticket model class

--- webapi/Model/Entity/Ticket.cs
+++ webapi/Model/Entity/Ticket.cs
@@ -8,7 +8,9 @@ namespace webapi.Model.Entity
         public long Id { get; set; }
         public Screening Screening { get; set; } = null!;
         public Seat Seat { get; set; } = null!;
+        public long SeatId { get; set; }
         public User User { get; set; } = null!;
+        public long UserId { get; set; }
         public bool Finalized { get; set; }
 
         public void SetFinalized()
