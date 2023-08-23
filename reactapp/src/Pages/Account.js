6009a2200d2b707561d88e6a6329fc3e5df45f97
Szajkoggg <116949920+Szajkoggg@users.noreply.github.com>
2023-08-23 10:17:35 +0200

    add account route and page

---
+++ reactapp/src/Pages/Account.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Paper } from "@mui/material";
+
+const Account = () => {
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        width: "100%",
+      }}
+    >
+      <Paper
+        sx={{
+          minWidth: "80%",
+          minHeight: 200,
+          margin: 3,
+        }}
+        elevation={3}
+      ></Paper>
+    </Container>
+  );
+};
+
+export default Account;
