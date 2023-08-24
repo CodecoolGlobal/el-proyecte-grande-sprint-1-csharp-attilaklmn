96797b546abb20147bfec0d4c7a8815f4bf7fb07
Csanyi Balint <113693078+mehty8@users.noreply.github.com>
2023-08-24 11:41:22 +0200

    DropDown menu

--- reactapp/src/Components/Program/Render.js
+++ reactapp/src/Components/Program/Render.js
@@ -6,6 +6,16 @@ const Render = ({ moviesScreened, allScreenings }) => {
 
   const navigate = useNavigate();
 
+  const handleSelectChange = (e) => {
+    const selectedDate = e.target.value;
+    const selectedScreening = allScreenings.find(x => x.startingDate === selectedDate);
+
+    if (selectedScreening) {
+      const { id: screeningId, roomId } = selectedScreening;
+      navigate(`/reservation/${screeningId}/${roomId}`);
+    }
+  };
+
   return moviesScreened.map((movie) => {
     return (
       <div className="ScreeningsForMovie" key={movie.id}>
@@ -14,24 +24,20 @@ const Render = ({ moviesScreened, allScreenings }) => {
         </div>
         <div className="title">{movie.title}</div>
         <div className="screeningTimes">
-          {"Screening Times:"}
-          {allScreenings
-            .filter((screening) => screening.movieId === movie.id)
-            .map((screening) => {
-              return (
-                <div key={screening.id}>
-                  <button onClick={() => {
-                    navigate(
-                      `/reservation/${screening.id}/${screening.roomId}`
-                    );
-                  }}>
-                  {`${screening.startingDate.split("T")[0]}  ${screening.startingDate.split("T")[1].replace(/(?<=\d{2}:\d{2}).{1,}/, "")}`}
-                  </button>
-                </div>
-              );
-            })}
+           <label htmlFor="screeningTimes">
+              <select name='ScreeningTimes' defaultValue="" onChange={handleSelectChange}>
+              <option value="" disabled>Select Screening Time</option>
+                 {allScreenings
+                  .filter((screening) => screening.movieId === movie.id)
+                  .map((screening) => {
+                    return (
+                      <option key={screening.id} value={screening.startingDate}>
+                        {`${screening.startingDate.split("T")[0]}  ${screening.startingDate.split("T")[1].replace(/(?<=\d{2}:\d{2}).{1,}/, "")}`}
+                      </option>)})}
+              </select>
+            </label>
+          </div>
         </div>
-      </div>
     );
   });
 };
