c418d4819ab260a3854a279bd8049eabc86d2505
Evelin Mandics <115423292+Maevenn@users.noreply.github.com>
2023-09-06 15:03:15 +0200

    sort movie list alphabetically

--- reactapp/src/Components/MovieList/MovieCard/MovieCard.js
+++ reactapp/src/Components/MovieList/MovieCard/MovieCard.js
@@ -6,34 +6,55 @@ import "./MovieCard.css";
 const MovieCard = ({ movieList }) => {
   const navigate = useNavigate();
 
-  return movieList.map((movie) => {
-    return (
-      <div className="movieCard" key={movie.id}>
-        <div className="movieImg">
-          <MovieCover movieTitle={movie.title.replace(" ", "+")} size="w300" />
-        </div>
-        <div className="movieDetails">
-          <div className="movieTitle">{movie.title}</div>
-          <div className="movieCast">
-            {movie.cast.map((star, index) => {
-              return <div key={movie.id + "-cast-" + index}>{star}</div>;
-            })}
+  return movieList
+    .sort((a, b) => {
+      const articles = ["a", "an", "the"];
+
+      const getTitleWithoutArticles = (title) => {
+        const splitTitle = title.split(" ");
+
+        if (articles.includes(splitTitle[0].toLowerCase())) {
+          return splitTitle.slice(1).join(" ");
+        }
+
+        return title;
+      };
+
+      const titleA = getTitleWithoutArticles(a.title.toLowerCase());
+      const titleB = getTitleWithoutArticles(b.title.toLowerCase());
+
+      return titleA.localeCompare(titleB);
+    })
+    .map((movie) => {
+      return (
+        <div className="movieCard" key={movie.id}>
+          <div className="movieImg">
+            <MovieCover
+              movieTitle={movie.title.replace(" ", "+")}
+              size="w300"
+            />
           </div>
-          <div className="movieSummary">{movie.summary}</div>
-          <div className="watchButton">
-            <Button
-              variant="contained"
-              onClick={() => {
-                navigate(`/program/${movie.id}`);
-              }}
-            >
-              Buy Ticket
-            </Button>
+          <div className="movieDetails">
+            <div className="movieTitle">{movie.title}</div>
+            <div className="movieCast">
+              {movie.cast.map((star, index) => {
+                return <div key={movie.id + "-cast-" + index}>{star}</div>;
+              })}
+            </div>
+            <div className="movieSummary">{movie.summary}</div>
+            <div className="watchButton">
+              <Button
+                onClick={() => {
+                  navigate(`/program/${movie.id}`);
+                }}
+              >
+                Buy Ticket
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
 };
 
 export default MovieCard;
